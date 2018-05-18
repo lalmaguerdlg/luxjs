fd35506bde42066ac322bc9d92da777cc4dc9904
Luis Almaguer <imaximuz@gmail.com>
2018-05-18 18:03:49 -0500

    game object class

--- src/Core/gameObject.js
+++ src/Core/gameObject.js
@@ -1,7 +1,42 @@
-
+import { Transform } from './transform'
 
 export class GameObject{
-    constructor(){
-        
+    constructor(parent){
+        this.parent = parent || undefined;
+        this.transform = new Transform();
+        if(this.parent){
+            this.transform.parent = this.parent.transform;
+        }
+        this.components = [];
+        this.input = undefined;
+    }
+
+    input() {
+
+    }
+
+    update() {
+
+    }
+
+    fixedUpdate() {
+
     }
 }
+
+
+class Component {
+    constructor(gameObject){
+        this.gameObject = gameObject;
+        this.transform = this.gameObject.transform;
+    }
+
+    update(){
+
+    }
+
+    fixedUpdate(){
+
+    }
+}
+
